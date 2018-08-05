234d87b659833cb9d85d7c5fa1c45515a07bea54
Mahdi H Mohamed <mr.mahdimohamediii@gmail.com>
2018-08-04 23:53:36 -0500

    added approve function, all tests passed
    added approve function with approve event, and approve mapping.

--- contracts/MahdiToken.sol
+++ contracts/MahdiToken.sol
@@ -1,61 +1,71 @@
-// version
+// Version
 pragma solidity ^0.4.2;
 
-// declare contract
+// declares the  contract
+//constuctor
 contract MahdiToken {
-	//constuctor
-
 	// Name
 	string public name ="MahdiToken";
-
 	// Symbol
 	string public symbol ="MAHDI";
-
 	//standard
 	string public standard ="MahdiToken v1.0";
 
-	
-	uint256 public totalSupply; //declare the state variable, public variable so solidity
-	// provides getter variable
-
+	//declare the state variable, public variable so solidity provides getter variable
+	uint256 public totalSupply; 
 	// transfer event
 	event Transfer(
 		address indexed _from, 
 		address indexed _to, 
 		uint256 _value
 	);
+	// approve event 
+	event Approval(
+		address indexed _owner,
+		address indexed _spender,
+		uint256 _value
+	);
 
-
-	// declare public cariable that gives us a reader function 
+	// declare public variable that gives us a reader function 
 	//declare a map, key is owner's address, with value being the account's balance 
 	mapping(address => uint256) public balanceOf;
+
+	// allowance 
+	mapping(address => mapping(address => uint256)) public allowance;
 
 	function MahdiToken(uint256 _initalSupply) public {
 		// Returns the account balance of an account with the given address
 		// msg.sender is the sender of the message (current call)
 		balanceOf[msg.sender] = _initalSupply;
-
 		// set the number of inital coins
 		totalSupply = _initalSupply;		
 	}
-
-
-
+	//transfer function
 	function transfer(address _to, uint256  _value) public returns (bool success) {
-
 	// trigger exception if the account balnce too low 
 	require(balanceOf[msg.sender] >= _value);
-
 	// Transfers MahdiToken  
 	balanceOf[msg.sender] -= _value;
 	balanceOf[_to] += _value;
-
 	//trigger a Transfer event
 	Transfer(msg.sender, _to, _value);
-
 	// return a boolean if above code worked
 	return true;
-
 	}
+
+	// approve function
+	function approve(address _spender, uint256 _value) public returns (bool success) {
+		// handles the allowance
+		allowance[msg.sender][_spender] = _value;
+
+		// approve event
+		Approval(msg.sender, _spender, _value);
+
+		return true;
+	}
+
+	// transferFrom function
+
+	//
 
 }
